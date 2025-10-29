0042555ba283172563032cb2ffcd46e9517a9eb4
Nicholas Hinke <hinke.nicholas@icloud.com>
2025-10-29 23:08:00 +0100

    added main page with upload component

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Upload from '$lib/components/Upload.svelte';
+</script>
+
+<div class="flex min-h-screen items-center justify-around">
+	<Upload />
+</div>
